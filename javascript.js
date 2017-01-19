42c54cb98c4aaada592acac7eafccb9d75b2f911
trevonwilliams227 <trevonwilliams227@gmail.com>
2017-01-19 23:26:44 +0000

    Init

---
+++ javascript.js
@@ -0,0 +1,15 @@
+ //$.getJSON(‘http://ajar-target.gomix.me/stops’, function(response){ })
+ $( document ).ready(function() {
+  $.getJSON("https://ajar-target.gomix.me/stops", function(response){ 
+   console.log(response)
+  })
+print()
+String N = "101 ";
+int T = uptown ;
+int S = Van Cortlandt Park;
+print(S);
+print(N, "T", S);
+
+  
+});
+  
